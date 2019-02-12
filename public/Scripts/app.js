03ab64938e37e99e5970c250d521bf1d4431996e
Michael <michaeljaison.me@gmail.com>
2019-02-11 22:51:18 -0500

    Delete function added for contacts

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -5,5 +5,13 @@
         "font-size: 20px; color: blue; font-weight: bold;");
     }
 
+    $(".btn-danger").click(function(event){
+        if(!confirm("Are you sure?")){
+            event.preventDefault();
+
+            window.location.assign("/contact-list");
+        }
+    });
+
     window.addEventListener("load", Start);
 })();
