d6a1e4ef14412d55c8da265cbc8fe6816a9e07cc
Michael <michaeljaison.me@gmail.com>
2019-02-11 22:15:16 -0500

    Create operation in contact model

--- routes/contact.js
+++ routes/contact.js
@@ -3,22 +3,46 @@ let router = express.Router();
 let mongoose = require('mongoose');
 
 //create reference to contactSchema model
-let contact = require('../models/contact');
+let contactModel = require('../models/contact');
 
 // Get Contact list - READ
 
 router.get('/', (req,res,next) => {
-    contact.find((err, contactList) => {
+    contactModel.find((err, contactList) => {
         if(err){
             return console.error(err);
         }else{
-            console.log(contactList);
+            // console.log(contactList);
             res.render('contacts/index', { 
-                title: 'My Contacts',
+                title: 'Contact List',
                 contacts: contactList 
             });
         }
     })
+});
+
+
+router.get('/add', (req,res,next) => {
+    res.render('contacts/add', { 
+        title: 'Add New Contact'
+    });
+});
+
+router.post('/add', (req,res,next) => {
+    let newContact = contactModel({
+        "firstName": req.body.firstName,
+        "lastName": req.body.lastName,
+        "age": req.body.age
+    });
+    contactModel.create(newContact, (err, contactModel)=>{
+        if(err){
+            console.log(err);
+            res.end(err);
+        }
+        else{
+            res.redirect("/contact-list");
+        }
+    });
 });
 
 module.exports = router;
